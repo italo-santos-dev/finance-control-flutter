d6883bd45926f424d88d741fc2ff1c11b4a47fa9
dev-italosantos <italotayllor@gmail.com>
2024-04-17 07:12:30 -0300

    feat: refinements in UI and data logic for stock quote chart Fixed alignment of the "Quote" text to the right, ensuring better visibility and consistency in the interface. Changed the CircularProgressIndicator color to grey[200] for smoother visual feedback during data loading. Implemented logic for handling and displaying historical stock data, including data preparation and processing for charts using FlSpot. Refactored the historical data fetch method, enhancing robustness and handling cases of missing data with clear console messages.

--- lib/pages/active/details/active_details_page.dart
+++ lib/pages/active/details/active_details_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_investment_control/models/active_model.dart';
 import 'package:flutter_investment_control/services/apis/api_stock_indicators.dart';
 import 'package:flutter_investment_control/services/apis/api_stocks_dividends.dart';
 import 'package:flutter_investment_control/services/apis/api_stocks_historicals.dart';
+import 'package:flutter_investment_control/widgets/btc/chart_page.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
@@ -26,6 +27,14 @@ class _ActiveDetailsPageState extends State<ActiveDetailsPage> {
   List<Map<String, dynamic>> dividendDataList = [];
   List<int> availableYears = [];
   int selectedYear = DateTime.now().year;
+
+  late Future<List<FlSpot>> _chartData;
+  Key _chartKey = UniqueKey(); // Chave única para o FutureBuilder
+
+  String formatCurrencyBRL(double value) {
+    final format = NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
+    return format.format(value);
+  }
 
   double _calculateFairValue() {
     if (_stockIndicators == null || _stockIndicators!['indicators'] == null) {
@@ -72,7 +81,32 @@ class _ActiveDetailsPageState extends State<ActiveDetailsPage> {
   void initState() {
     super.initState();
     _fetchStockIndicators();
+    _chartData = _fetchChartData();
     fetchData();
+  }
+
+  Future<List<FlSpot>> _fetchChartData({int months = 3}) async {
+    var stockData =
+    await StocksHistoricals().getStockHistoricals(widget.active.symbol, months);
+    List<dynamic> historicals = stockData?['historicals'] ?? [];
+
+    if (historicals.isEmpty) {
+      print("Nenhum dado histórico recebido para os últimos $months meses.");
+      return [];
+    }
+
+    List<FlSpot> chartData = [];
+    for (var i = 0; i < historicals.length; i++) {
+      DateTime date = DateTime.parse(historicals[i]['date']);
+      double close = (historicals[i]['close'] as num).toDouble();
+      chartData.add(FlSpot(date.millisecondsSinceEpoch.toDouble(), close));
+    }
+
+    print(
+        "Recebidos ${historicals.length} pontos de dados da API para os últimos $months meses.");
+    print(
+        "Processados ${chartData.length} pontos de dados FlSpot para o gráfico.");
+    return chartData;
   }
 
   Future<List<double>> _calculateReturns() async {
@@ -268,6 +302,50 @@ class _ActiveDetailsPageState extends State<ActiveDetailsPage> {
                       ),
                     ],
                   ),
+                  Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft, // Ajusta o alinhamento para direita
+                        child: const Text(
+                          'Cotação',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      _buildFilterButtons(),
+                      FutureBuilder<List<FlSpot>>(
+                        key: _chartKey, // Usa a chave que muda a cada novo futuro
+                        future: _chartData,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting && !snapshot.hasData) {
+                            return Center(child: CircularProgressIndicator(color: Colors.grey[900]));
+                          } else if (snapshot.connectionState == ConnectionState.waiting) {
+                            // Continua mostrando o gráfico antigo e adiciona uma mensagem de que está atualizando
+                            return Stack(
+                              children: [
+                                _buildChart(snapshot.data!),
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: Container(
+                                    color: Colors.black54,
+                                    padding: EdgeInsets.all(8),
+                                    child: Text('Atualizando...', style: TextStyle(color: Colors.white)),
+                                  ),
+                                ),
+                              ],
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(child: Text('Erro: ${snapshot.error}'));
+                          } else {
+                            return _buildChart(snapshot.data!);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
                   _buildIndicatorsSection(),
                   const SizedBox(height: 15),
                   _buildFairValueSection(),
@@ -530,7 +608,7 @@ class _ActiveDetailsPageState extends State<ActiveDetailsPage> {
 
   Widget _buildIndicatorsSection() {
     if (_stockIndicators == null || _stockIndicators!['indicators'] == null) {
-      return Center(child: CircularProgressIndicator());
+      return Container();
     }
 
     final List<dynamic>? indicatorsDynamic = _stockIndicators!['indicators'];
@@ -698,6 +776,138 @@ class _ActiveDetailsPageState extends State<ActiveDetailsPage> {
         ),
       ),
     );
+  }
+
+  Widget _buildFilterButtons() {
+    final List<Map<String, dynamic>> filters = [
+      {'label': '30D', 'months': 1},
+      {'label': '6M', 'months': 6},
+      {'label': '1A', 'months': 12},
+      {'label': '5A', 'months': 60},
+    ];
+
+    void _applyFilter(int months) {
+      print("Filtro Aplicado: $months meses");
+      setState(() {
+        _chartData = _fetchChartData(months: months);
+        _chartKey = UniqueKey();  // Atualiza a chave para forçar reconstrução do FutureBuilder
+      });
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: filters.map((filter) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8), // Adiciona espaço entre os botões
+              child: ElevatedButton(
+                onPressed: () => _applyFilter(filter['months']),
+                child: Text(filter['label'], style: TextStyle(color: Colors.white)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[850],  // dark grey button background
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // rounded corners
+                  ),
+                  elevation: 3,  // subtle shadow
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildChart(List<FlSpot> chartData) {
+    return Container(
+      height: 300, // Define a altura para metade da altura da tela
+      padding:
+      EdgeInsets.only(right: 20, left: 16), // Adicionado padding à esquerda
+      child: LineChart(
+        LineChartData(
+          gridData: FlGridData(show: false), // Desativa a grade
+          titlesData: FlTitlesData(
+            bottomTitles: SideTitles(
+              showTitles: true,
+              reservedSize: 22,
+              getTextStyles: (context, value) => const TextStyle(
+                color: Colors.blueGrey,
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
+              getTitles: (value) {
+                return getTitleForValue(value, chartData); // Usa a função personalizada para obter títulos
+              },
+              margin: 10,
+            ),            leftTitles: SideTitles(
+            showTitles: true,
+            getTitles: (value) {
+              return formatCurrencyBRL(value); // Formatação para BRL
+            },
+            getTextStyles: (context, value) => const TextStyle(
+              color:
+              Colors.blueGrey, // Cor azul-cinza para os títulos do eixo Y
+              fontSize: 12, // Tamanho de fonte conforme sua especificação
+            ),
+            reservedSize: 40, // Espaço suficiente ajustado para legibilidade
+            margin: 12,// Margem mantida
+          ),
+            rightTitles: SideTitles(showTitles: false),
+            topTitles: SideTitles(showTitles: false),
+          ),
+          borderData: FlBorderData(
+            show: true,
+            border: Border.all(color: const Color(0xffe7e8ec), width: 1),
+          ),
+          minX: chartData.first.x,
+          maxX: chartData.last.x,
+          minY: chartData.map((spot) => spot.y).reduce(min) * 0.9,
+          maxY: chartData.map((spot) => spot.y).reduce(max) * 1.1,
+          lineBarsData: [
+            LineChartBarData(
+              spots: chartData,
+              isCurved: true,
+              colors: [
+                Colors.grey[900]!
+              ], // Utiliza uma lista de cores, e faz unwrap do opcional
+              barWidth: 2,
+              isStrokeCapRound: true,
+              dotData: FlDotData(show: false),
+              belowBarData: BarAreaData(show: false),
+            ),
+          ],
+          lineTouchData: LineTouchData(
+            touchTooltipData: LineTouchTooltipData(
+              tooltipBgColor: Colors.blueGrey, // Fundo do tooltip
+              tooltipPadding: const EdgeInsets.all(8),
+              tooltipMargin: 5,
+              getTooltipItems: (List<LineBarSpot> touchedSpots) {
+                return touchedSpots.map((touchedSpot) {
+                  final textStyle = TextStyle(
+                    color: Colors.white, // Cor do texto para branco
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  );
+                  return LineTooltipItem(
+                      formatCurrencyBRL(touchedSpot.y), textStyle);
+                }).toList();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  String getTitleForValue(double value, List<FlSpot> chartData) {
+    // Find the closest data point, because exact matches of timestamps might not be aligned.
+    var closestSpot = chartData.reduce((a, b) => (value - a.x).abs() < (value - b.x).abs() ? a : b);
+    var date = DateTime.fromMillisecondsSinceEpoch(closestSpot.x.toInt());
+    return DateFormat('MMM dd').format(date); // You can adjust the date format here
   }
 
   List<Widget> _buildIndicatorCards(
--- lib/widgets/btc/chart_page.dart
+++ lib/widgets/btc/chart_page.dart
@@ -1,148 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:fl_chart/fl_chart.dart';
-// import 'package:flutter_investment_control/services/apis/api_stocks_historicals.dart';
-// import 'package:intl/intl.dart';
-// import 'dart:math'; // Add this line to use min and max
-//
-// class ChartPage extends StatefulWidget {
-//   final String ticker;
-//
-//   ChartPage({Key? key, required this.ticker}) : super(key: key);
-//
-//   @override
-//   _ChartPageState createState() => _ChartPageState();
-// }
-//
-// class _ChartPageState extends State<ChartPage> {
-//   late Future<List<FlSpot>> _chartData;
-//
-//   String formatCurrencyBRL(double value) {
-//     final format = NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
-//     return format.format(value);
-//   }
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _chartData = _fetchChartData();
-//   }
-//
-//   Future<List<FlSpot>> _fetchChartData() async {
-//     var stockData = await StocksHistoricals().getStockHistoricals(widget.ticker);
-//     List<dynamic> historicals = stockData?['historicals'] ?? [];
-//
-//     List<FlSpot> chartData = [];
-//     for (var i = 0; i < historicals.length; i++) {
-//       DateTime date = DateTime.parse(historicals[i]['date']);
-//       double close = (historicals[i]['close'] as num).toDouble();
-//       chartData.add(FlSpot(date.millisecondsSinceEpoch.toDouble(), close));
-//     }
-//
-//     return chartData;
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Stock Price Chart'),
-//       ),
-//       body: FutureBuilder<List<FlSpot>>(
-//         future: _chartData,
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return Center(child: CircularProgressIndicator());
-//           } else if (snapshot.hasError) {
-//             return Center(child: Text('Error: ${snapshot.error}'));
-//           } else {
-//             return _buildChart(snapshot.data!);
-//           }
-//         },
-//       ),
-//     );
-//   }
-//
-//   Widget _buildChart(List<FlSpot> chartData) {
-//     return Container(
-//       height: MediaQuery.of(context).size.height / 2, // Define a altura para metade da altura da tela
-//       padding: EdgeInsets.only(right: 20),  // Reduz o espaço à direita do gráfico
-//       child: LineChart(
-//         LineChartData(
-//           gridData: FlGridData(show: false), // Desativa a grade
-//           titlesData: FlTitlesData(
-//             bottomTitles: SideTitles(
-//               showTitles: true,
-//               reservedSize: 22,
-//               getTextStyles: (context, value) => const TextStyle(
-//                 color: Colors.blueGrey, // Cor azul-cinza para os títulos do eixo X
-//                 fontWeight: FontWeight.bold,
-//                 fontSize: 12, // Tamanho de fonte conforme sua especificação
-//               ),
-//               getTitles: (value) {
-//                 DateTime date = DateTime.fromMillisecondsSinceEpoch(value.toInt());
-//                 return DateFormat('MMM d').format(date);  // Formato mais limpo e moderno para a data
-//               },
-//               margin: 10,  // Margem ajustada como no exemplo
-//             ),
-//             leftTitles: SideTitles(
-//               showTitles: true,
-//               getTitles: (value) {
-//                 return '\$${value.toStringAsFixed(2)}';
-//               },
-//               getTextStyles: (context, value) => const TextStyle(
-//                 color: Colors.blueGrey, // Cor azul-cinza para os títulos do eixo Y
-//                 fontSize: 12, // Tamanho de fonte conforme sua especificação
-//               ),
-//               reservedSize: 40,  // Espaço suficiente ajustado para legibilidade
-//               margin: 12,  // Margem mantida
-//             ),
-//             rightTitles: SideTitles(showTitles: false),
-//             topTitles: SideTitles(showTitles: false),
-//           ),
-//           borderData: FlBorderData(
-//             show: true,
-//             border: Border.all(color: const Color(0xffe7e8ec), width: 1),
-//           ),
-//           minX: chartData.first.x,
-//           maxX: chartData.last.x,
-//           minY: chartData.map((spot) => spot.y).reduce(min) * 0.9,
-//           maxY: chartData.map((spot) => spot.y).reduce(max) * 1.1,
-//           lineBarsData: [
-//             LineChartBarData(
-//               spots: chartData,
-//               isCurved: true,
-//               colors: [Colors.grey[900]!], // Utiliza uma lista de cores, e faz unwrap do opcional
-//               barWidth: 2,
-//               isStrokeCapRound: true,
-//               dotData: FlDotData(show: false),
-//               belowBarData: BarAreaData(show: false),
-//             ),
-//           ],
-//           lineTouchData: LineTouchData(
-//             touchTooltipData: LineTouchTooltipData(
-//               tooltipBgColor: Colors.blueGrey, // Fundo do tooltip
-//               tooltipPadding: const EdgeInsets.all(8),
-//               tooltipMargin: 5,
-//               getTooltipItems: (List<LineBarSpot> touchedSpots) {
-//                 return touchedSpots.map((touchedSpot) {
-//                   final textStyle = TextStyle(
-//                     color: Colors.white, // Cor do texto para branco
-//                     fontWeight: FontWeight.bold,
-//                     fontSize: 14,
-//                   );
-//                   return LineTooltipItem(
-//                       '\$${touchedSpot.y.toStringAsFixed(2)}', textStyle
-//                   );
-//                 }).toList();
-//               },
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_investment_control/services/apis/api_stocks_historicals.dart';
@@ -174,7 +29,8 @@ class _ChartPageState extends State<ChartPage> {
   }
 
   Future<List<FlSpot>> _fetchChartData({int months = 3}) async {
-    var stockData = await StocksHistoricals().getStockHistoricals(widget.ticker, months);
+    var stockData =
+        await StocksHistoricals().getStockHistoricals(widget.ticker, months);
     List<dynamic> historicals = stockData?['historicals'] ?? [];
 
     if (historicals.isEmpty) {
@@ -189,76 +45,108 @@ class _ChartPageState extends State<ChartPage> {
       chartData.add(FlSpot(date.millisecondsSinceEpoch.toDouble(), close));
     }
 
-    print("Recebidos ${historicals.length} pontos de dados da API para os últimos $months meses.");
-    print("Processados ${chartData.length} pontos de dados FlSpot para o gráfico.");
+    print(
+        "Recebidos ${historicals.length} pontos de dados da API para os últimos $months meses.");
+    print(
+        "Processados ${chartData.length} pontos de dados FlSpot para o gráfico.");
     return chartData;
   }
 
-  @override
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Stock Price Chart'),
-      ),
       body: Column(
         children: [
           _buildFilterButtons(), // Botões de filtro
-          Expanded(
-            child: FutureBuilder<List<FlSpot>>(
-              key: _chartKey, // Usa a chave que muda a cada novo futuro
-              future: _chartData,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
-                  return _buildChart(snapshot.data!);
-                }
-              },
-            ),
-          ),        ],
+          FutureBuilder<List<FlSpot>>(
+            key: _chartKey, // Usa a chave que muda a cada novo futuro
+            future: _chartData,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting && !snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                // Continua mostrando o gráfico antigo e adiciona uma mensagem de que está atualizando
+                return Stack(
+                  children: [
+                    _buildChart(snapshot.data!),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        color: Colors.black54,
+                        padding: EdgeInsets.all(8),
+                        child: Text('Atualizando...', style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Erro: ${snapshot.error}'));
+              } else {
+                return _buildChart(snapshot.data!);
+              }
+            },
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildFilterButtons() {
     final List<Map<String, dynamic>> filters = [
-      {'label': '1 MÊS', 'months': 1},
-      {'label': '3 MESES', 'months': 3},
-      {'label': '6 MESES', 'months': 6},
-      {'label': '1 ANO', 'months': 12},
-      {'label': '5 ANOS', 'months': 60},
+      {'label': '30D', 'months': 1},
+      {'label': '6M', 'months': 6},
+      {'label': '1A', 'months': 12},
+      {'label': '5A', 'months': 60},
     ];
 
     void _applyFilter(int months) {
       print("Filtro Aplicado: $months meses");
       setState(() {
         _chartData = _fetchChartData(months: months);
-        _chartKey = UniqueKey(); // Atualiza a chave para forçar reconstrução do FutureBuilder
+        _chartKey = UniqueKey();  // Atualiza a chave para forçar reconstrução do FutureBuilder
       });
     }
 
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      padding: EdgeInsets.symmetric(vertical: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: filters.map((filter) {
-          return ElevatedButton(
-            onPressed: () => _applyFilter(filter['months']),
-            child: Text(filter['label']),
-          );
-        }).toList(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: filters.map((filter) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8), // Adiciona espaço entre os botões
+              child: ElevatedButton(
+                onPressed: () => _applyFilter(filter['months']),
+                child: Text(filter['label'], style: TextStyle(color: Colors.white)),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[850],  // dark grey button background
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20), // rounded corners
+                  ),
+                  elevation: 3,  // subtle shadow
+                ),
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
 
+  String getTitleForValue(double value, List<FlSpot> chartData) {
+    // Find the closest data point, because exact matches of timestamps might not be aligned.
+    var closestSpot = chartData.reduce((a, b) => (value - a.x).abs() < (value - b.x).abs() ? a : b);
+    var date = DateTime.fromMillisecondsSinceEpoch(closestSpot.x.toInt());
+    return DateFormat('MMM dd').format(date); // You can adjust the date format here
+  }
+
+
   Widget _buildChart(List<FlSpot> chartData) {
     return Container(
-      height: MediaQuery.of(context).size.height /
-          2, // Define a altura para metade da altura da tela
+      height: 300, // Define a altura para metade da altura da tela
       padding:
           EdgeInsets.only(right: 20, left: 16), // Adicionado padding à esquerda
       child: LineChart(
@@ -269,20 +157,15 @@ class _ChartPageState extends State<ChartPage> {
               showTitles: true,
               reservedSize: 22,
               getTextStyles: (context, value) => const TextStyle(
-                color:
-                    Colors.blueGrey, // Cor azul-cinza para os títulos do eixo X
+                color: Colors.blueGrey,
                 fontWeight: FontWeight.bold,
-                fontSize: 12, // Tamanho de fonte conforme sua especificação
+                fontSize: 12,
               ),
               getTitles: (value) {
-                DateTime date =
-                    DateTime.fromMillisecondsSinceEpoch(value.toInt());
-                return DateFormat('MMM d')
-                    .format(date); // Formato mais limpo e moderno para a data
+                return getTitleForValue(value, chartData); // Usa a função personalizada para obter títulos
               },
-              margin: 10, // Margem ajustada como no exemplo
-            ),
-            leftTitles: SideTitles(
+              margin: 10,
+            ),            leftTitles: SideTitles(
               showTitles: true,
               getTitles: (value) {
                 return formatCurrencyBRL(value); // Formatação para BRL
@@ -293,7 +176,7 @@ class _ChartPageState extends State<ChartPage> {
                 fontSize: 12, // Tamanho de fonte conforme sua especificação
               ),
               reservedSize: 40, // Espaço suficiente ajustado para legibilidade
-              margin: 12, // Margem mantida
+              margin: 12,// Margem mantida
             ),
             rightTitles: SideTitles(showTitles: false),
             topTitles: SideTitles(showTitles: false),
